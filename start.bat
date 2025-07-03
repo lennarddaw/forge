d7fde13cf992a0b487ca4f21ff240f3f01856423
lennarddaw <lennardgross07@gmail.com>
2025-07-03 10:06:11 +0200

    Update start.bat to clarify project directory change comment

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 @echo off
-REM ins Projektverzeichnis wechseln
+REM ins Projektverzeichnis
 cd /d "%~dp0"
 REM Server starten
 py -m uvicorn backend.verify_api:app --reload --host 127.0.0.1 --port 8000
